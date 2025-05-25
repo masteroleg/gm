82f9717fdbe0d29cc27292ccb9e9752e7ba2048b
masteroleg <oleg.romanov@gmail.com>
2025-05-26 00:31:46 +0300

    Add mobile menu functionality with open/close event listeners

---
+++ assets/js/menu.js
@@ -0,0 +1,9 @@
+// --- Mobile menu open/close ---
+document.getElementById('burgerBtn').addEventListener('click', function () {
+	document.getElementById('mainNav').classList.remove('hidden');
+});
+// Закрытие меню
+document.getElementById('closeMenu').addEventListener('click', function () {
+	document.getElementById('mainNav').classList.add('hidden');
+});
+  
